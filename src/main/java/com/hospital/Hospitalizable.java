677f4aa338589f56aa13c3fcbeed30630190b1cb
gzifoto-spec <gzifoto@gmail.com>
2026-02-27 12:46:00 +0100

    feat: add Hospitalizable interface

---
+++ src/main/java/com/hospital/Hospitalizable.java
@@ -0,0 +1,10 @@
+package com.hospital;
+
+public interface Hospitalizable {
+
+    public abstract void admission();
+    public abstract void assignRoom();
+    public abstract void treatment();
+    public abstract void discharge();
+
+}
