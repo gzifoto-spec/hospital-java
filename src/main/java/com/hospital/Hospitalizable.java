79532ef4a640e874582521178183c3463fe57dbf
gzifoto-spec <gzifoto@gmail.com>
2026-03-03 12:24:15 +0100

    refactor: remove redundant public abstract from interface

--- src/main/java/com/hospital/Hospitalizable.java
+++ src/main/java/com/hospital/Hospitalizable.java
@@ -2,9 +2,9 @@ package com.hospital;
 
 public interface Hospitalizable {
 
-    public abstract void admission();
-    public abstract void assignRoom();
-    public abstract void treatment();
-    public abstract void discharge();
+    void admission();
+    void assignRoom();
+    void treatment();
+    void discharge();
 
 }
