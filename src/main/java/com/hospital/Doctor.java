3f7e4051ec10f1f4ddfd7a267db5f3a86811d26b
gzifoto-spec <gzifoto@gmail.com>
2026-03-03 09:04:31 +0100

    refactor: add weeklyHours attribute to Doctor

--- src/main/java/com/hospital/Doctor.java
+++ src/main/java/com/hospital/Doctor.java
@@ -3,10 +3,12 @@ package com.hospital;
 public abstract class Doctor extends Character implements Workable {
 
     private String employeeCode;
+    private int weeklyHours;
 
-    public Doctor(String name, int age, String idNumber, String employeeCode) {
+    public Doctor(String name, int age, String idNumber, String employeeCode, int weeklyHours) {
         super(name, age, idNumber);
         this.employeeCode = employeeCode;
+        this.weeklyHours = weeklyHours;
     }
 
     public String getEmployeeCode() {
@@ -16,4 +18,13 @@ public abstract class Doctor extends Character implements Workable {
     public void setEmployeeCode(String employeeCode) {
         this.employeeCode = employeeCode;
     }
+
+    public int getWeeklyHours() {
+        return weeklyHours;
+    }
+
+    public void setWeeklyHours(int weeklyHours) {
+        this.weeklyHours = weeklyHours;
+    }
+
 }
