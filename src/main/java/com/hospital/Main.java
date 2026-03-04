ff4f04d9fa7b8d462d6ea58de1e709c07777ae97
gzifoto-spec <gzifoto@gmail.com>
2026-03-04 09:31:06 +0100

    feat: print character info via toString in main loops

--- src/main/java/com/hospital/Main.java
+++ src/main/java/com/hospital/Main.java
@@ -11,6 +11,9 @@ public class Main {
         List<AmbulancePatient> ambulancePatients = List.of(patient01, patient02);
 
         for(AmbulancePatient patient : ambulancePatients) {
+            System.out.println();
+            System.out.println(patient);
+            System.out.println("-----------------------------------------------------------------");
             patient.admission();
             patient.operation();
             patient.assignRoom();
@@ -25,6 +28,9 @@ public class Main {
         List<ConsultationPatient> consultationPatients = List.of(patient03, patient04);
 
         for(ConsultationPatient patient : consultationPatients) {
+            System.out.println();
+            System.out.println(patient);
+            System.out.println("-----------------------------------------------------------------");
             patient.admission();
             patient.assignRoom();
             patient.treatment();
@@ -33,26 +39,33 @@ public class Main {
             patient.nextAppointment();
         }
 
-        SpecialistDoctor doctor01 = new SpecialistDoctor("JM", 40, "22543876Q", "masterOfThePHP", "Urólogo", 40);
-        SpecialistDoctor doctor02 = new SpecialistDoctor("Ana Aragón", 30, "44123987S", "becauseIamHappy", "Psiquiatra", 40);
+        SpecialistDoctor doctor01 = new SpecialistDoctor("JM", 40, "22543876Q", "'masterOfThePHP'", "Urología", 40);
+        SpecialistDoctor doctor02 = new SpecialistDoctor("Ana Aragón", 30, "44123987S", "'becauseIamHappy'", "Psiquiatría", 40);
 
         List<SpecialistDoctor> specialistDoctors = List.of(doctor01, doctor02);
 
         for (SpecialistDoctor doctor : specialistDoctors) {
+            System.out.println();
+            System.out.println(doctor);
+            System.out.println("-----------------------------------------------------------------------------------------------------");
             doctor.weeklyWorkHours();
             doctor.eat();
             doctor.vacation();
         }
 
-        StudentDoctor student01 = new StudentDoctor("JuanLu", 42, "54678321G","soyDeBackend", 20, "Radiología", 20);
-        StudentDoctor student02 = new StudentDoctor("David Navarro", 45, "36789123H", "archiMegaTester", 20, "Otorrinolaringología", 20);
+        StudentDoctor student01 = new StudentDoctor("JuanLu", 42, "54678321G","'soyDeBackend'", 20, "Radiología", 20);
+        StudentDoctor student02 = new StudentDoctor("David Navarro", 45, "36789123H", "'archiMegaTester'", 20, "Otorrinolaringología", 20);
 
         List<StudentDoctor> studentDoctors = List.of(student01, student02);
 
         for (StudentDoctor student : studentDoctors) {
+            System.out.println();
+            System.out.println(student);
+            System.out.println("-------------------------------------------------------------------------------------------------------------");
             student.weeklyWorkHours();
             student.weeklyStudyHours();
             student.eat();
+            System.out.println();
         }
     }
 }
